262187219784b037dc18d6925b4590adb1e4b355
Nick-Nunley <nnunley@mednet.ucla.edu>
2025-05-04 18:12:39 -0700

    Added a contact header to CV.PDF

--- src/CvExport.py
+++ src/CvExport.py
@@ -1,10 +1,11 @@
 import argparse
 import requests
 from weasyprint import HTML
+from bs4 import BeautifulSoup
 
 class DigitalCV:
 
-    def __init__(self, url: str, html: str = None, output_path: str = None, style_config: str = '_sass/override.scss'):
+    def __init__(self, url: str, html: str = None, output_path: str = None, style_config: str = '_sass/override.scss', contact_html: str = None):
         self.url = url
         if html is None:
             html = ''
@@ -13,17 +14,42 @@ class DigitalCV:
             output_path = 'output.pdf'
         self.output_path = output_path
         self.style_config = style_config
+        if contact_html is None:
+            contact_html = f"""
+                <div style="display: flex; justify-content: space-between; align-items: baseline; border-bottom: 1px solid #ccc; padding-bottom: 0.25em; margin-bottom: 1em;">
+                    <div style="font-size: 16pt; font-weight: 700; margin: 0;">CV</div>
+                    <div style="font-size: 10pt;">
+                        <a href="https://nick-nunley.github.io/PersonalWebsite/"><strong>Nicholas M. Nunley</strong></a> &nbsp;|&nbsp;
+                        <a href="mailto:nicknunley17@ucla.edu">nicknunley17@ucla.edu</a> &nbsp;|&nbsp;
+                        <a href="https://github.com/Nick-Nunley">GitHub</a> &nbsp;|&nbsp;
+                        <a href="https://www.linkedin.com/in/nicholas-nunley/">Linkedin</a> &nbsp;|&nbsp;
+                        Los Angeles, CA
+                    </div>
+                </div>
+                """
+        self.contact_html = contact_html
 
     def make_request(self, url: str) -> str:
         response = requests.get(url)
         response.raise_for_status()
         return response.text
 
+    def inject_contact_info(self, html: str, contact_html: str) -> str:
+        soup = BeautifulSoup(html, 'html.parser')
+        # Find the 'CV' header element
+        cv_header = soup.find('h3', string='CV')
+        if cv_header:
+            # Create a combined HTML block inside a new container
+            merged_block = merged_block = BeautifulSoup(self.contact_html, 'html.parser')
+            cv_header.replace_with(merged_block)
+        return str(soup)
+
     def render_pdf(self, html: str) -> None:
         HTML(string = html).write_pdf(self.output_path, stylesheets = [self.style_config])
 
     def main(self) -> None:
         self.html = self.make_request(url = self.url)
+        self.html = self.inject_contact_info(html = self.html, contact_html = self.contact_html)
         self.render_pdf(html = self.html)
 
 
